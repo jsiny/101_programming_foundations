40233b2b6b895f11f9695459ce168ed4e7e0050f
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-06 08:49:38 +0000

    :ok_hand: Add new method to solve ex 7.2

--- exercises/7_easy_6/ex_02.rb
+++ exercises/7_easy_6/ex_02.rb
@@ -7,3 +7,13 @@ end
 p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
 p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
 p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+# Further Exploration
+
+def remove_vowels_2(array)
+  array.map { |string| string.delete('aeiouAEIOU') }
+end
+
+p remove_vowels_2(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+p remove_vowels_2(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+p remove_vowels_2(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
