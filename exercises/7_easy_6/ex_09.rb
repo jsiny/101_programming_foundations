2fb13e95b8a56bfb70415f8ceb7f76d653627e46
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-09 18:08:23 +0000

    :ok_hand: Add new method for ex 7.9

--- exercises/7_easy_6/ex_09.rb
+++ exercises/7_easy_6/ex_09.rb
@@ -8,3 +8,15 @@ p include?([1, 2, 3, 4, 5], 6) == false
 p include?([], 3) == false
 p include?([nil], nil) == true
 p include?([], nil) == false
+
+# Add quicker solution
+
+def include_2?(array, search)
+  !!array.find_index(search)
+end
+
+p include_2?([1, 2, 3, 4, 5], 3) == true
+p include_2?([1, 2, 3, 4, 5], 6) == false
+p include_2?([], 3) == false
+p include_2?([nil], nil) == true
+p include_2?([], nil) == false
