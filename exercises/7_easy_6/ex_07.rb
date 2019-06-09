0c3e43372baeb8a1f0dd0ae46dc44aa618c92b47
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-09 06:19:22 +0000

    :ok_hand: Add new method to solve ex 7.7

--- exercises/7_easy_6/ex_07.rb
+++ exercises/7_easy_6/ex_07.rb
@@ -31,3 +31,17 @@ p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
 p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
 p halvsies([5]) == [[5], []]
 p halvsies([]) == [[], []]
+
+# Better solution
+
+def halvsies_2(array)
+  middle = (array.size / 2.0).ceil
+  first_half = array.slice(0, middle)
+  second_half = array.slice(middle, array.size - middle)
+  [first_half, second_half]
+end
+
+p halvsies_2([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+p halvsies_2([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+p halvsies_2([5]) == [[5], []]
+p halvsies_2([]) == [[], []]
