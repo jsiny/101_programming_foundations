d77b8b37846d4bc24a3d7b5316fc9ceb8fe82b30
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-10 06:34:53 +0000

    :sparkles: Add general case for triangle generation in ex  7.10

--- exercises/7_easy_6/ex_10.rb
+++ exercises/7_easy_6/ex_10.rb
@@ -3,6 +3,7 @@ def triangle(size)
 end
 
 triangle(5)
+puts '------------'
 triangle(9)
 
 # Further exploration
@@ -11,4 +12,24 @@ def triangle_upside_down(size)
   (0..(size - 1)).each { |white| puts '*' * (size - white) + ' ' * white }
 end
 
+puts '------------'
 triangle_upside_down(5)
+puts '------------'
+
+def triangle_anywhere(n, corner)
+  case corner
+  when 'down_right' then (1..n).each { |s| puts ' ' * (n - s) + '*' * s }
+  when 'up_left' then (0..(n - 1)).each { |w| puts '*' * (n - w) + ' ' * w }
+  when 'down_left' then (1..n).each { |s| puts '*' * s + ' ' * (n - s) }
+  when 'up_right' then (0..(n - 1)).each { |w| puts ' ' * w + '*' * (n - w) }
+  else puts "I didn't understand your input, sorry!"
+  end
+end
+
+triangle_anywhere(3, 'down_right')
+puts '------------'
+triangle_anywhere(3, 'up_left')
+puts '------------'
+triangle_anywhere(3, 'down_left')
+puts '------------'
+triangle_anywhere(3, 'up_right')
