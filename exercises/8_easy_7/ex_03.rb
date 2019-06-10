ac3917012c488ab69acabd38c1c70935fe2e1678
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-10 16:23:02 +0000

    :ok_hand: Add a new way to capitalize a string in ex 8.3

--- exercises/8_easy_7/ex_03.rb
+++ exercises/8_easy_7/ex_03.rb
@@ -5,3 +5,16 @@ end
 p word_cap('four score and seven') == 'Four Score And Seven'
 p word_cap('the javaScript language') == 'The Javascript Language'
 p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
+
+# Further Exploration
+# How to capitalize without the capitalize method
+
+def word_cap_2(string)
+  array = []
+  string.split.each { |word| array << word[0].upcase + word[1..-1].downcase }
+  array.join(' ')
+end
+
+p word_cap_2('four score and seven') == 'Four Score And Seven'
+p word_cap_2('the javaScript language') == 'The Javascript Language'
+p word_cap_2('this is a "quoted" word') == 'This Is A "quoted" Word'
