b4264696c42995c9a416c2716ba1a6791a7c3f5c
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-11 18:53:40 +0000

    :ok_hand: Add new method for 8.9

--- exercises/8_easy_7/ex_09.rb
+++ exercises/8_easy_7/ex_09.rb
@@ -12,3 +12,12 @@ end
 
 p multiply_all_pairs([1, 2], [2, 3, 4]) == [2, 3, 4, 4, 6, 8]
 p multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
+
+# Shorter method
+
+def multiply_all_pairs_2(arr1, arr2)
+  arr1.product(arr2).map { |x, y| x * y }.sort
+end
+
+p multiply_all_pairs_2([1, 2], [2, 3, 4]) == [2, 3, 4, 4, 6, 8]
+p multiply_all_pairs_2([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
