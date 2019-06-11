c151d298f375b81212928f443520f574dad7d5c4
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-11 08:02:03 +0000

    :ok_hand: Add shorter method for ex 8.8

--- exercises/8_easy_7/ex_08.rb
+++ exercises/8_easy_7/ex_08.rb
@@ -5,3 +5,11 @@ def multiply_list(arr1, arr2)
 end
 
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+# Further Exploration
+
+def multiply_list_2(arr1, arr2)
+  arr1.zip(arr2).map { |arr| arr.inject(:*) }
+end
+
+p multiply_list_2([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
