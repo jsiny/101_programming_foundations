027e2a842d9143a9dc52d367426946a0de009c8e
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-11 07:49:51 +0000

    :sparkles: Add ex 8.8

--- exercises/8_easy_7/ex_08.rb
+++ exercises/8_easy_7/ex_08.rb
@@ -1,1 +1,7 @@
-# DERNIER EXERCICE A FAIRE AVANT DE PASSER A LA PROCHAINE LECON
+def multiply_list(arr1, arr2)
+  result = []
+  arr1.each_with_index { |n, i| result << n * arr2[i] }
+  result
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
