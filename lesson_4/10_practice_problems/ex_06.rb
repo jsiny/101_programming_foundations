ffb55537ede9fc27fae9b9683af52560e057680d
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-11 18:41:54 +0000

    :sparkles: Add practice problem 10.10

--- lesson_4/10_practice_problems/ex_06.rb
+++ lesson_4/10_practice_problems/ex_06.rb
@@ -1,4 +1,3 @@
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 
 p flintstones.map! { |name| name[0, 3] }
-
---
+++ lesson_4/10_practice_problems/ex_10.rb
@@ -0,0 +1,25 @@
+munsters = {
+  'Herman' => { 'age' => 32, 'gender' => 'male' },
+  'Lily' => { 'age' => 30, 'gender' => 'female' },
+  'Grandpa' => { 'age' => 402, 'gender' => 'male' },
+  'Eddie' => { 'age' => 10, 'gender' => 'male' },
+  'Marilyn' => { 'age' => 23, 'gender' => 'female' }
+}
+
+munsters.each_key do |name|
+  person = munsters[name]
+
+  person['age_group'] = case person['age']
+                        when (0..17) then 'kid'
+                        when (18..64) then 'adult'
+                        else 'senior'
+                        end
+end
+
+p munsters
+
+# { 'Herman' => { 'age' => 32, 'gender' => 'male', 'age_group' => 'adult' },
+#   'Lily' => {'age' => 30, 'gender' => 'female', 'age_group' => 'adult' },
+#   'Grandpa' => { 'age' => 402, 'gender' => 'male', 'age_group' => 'senior' },
+#   'Eddie' => { 'age' => 10, 'gender' => 'male', 'age_group' => 'kid' },
+#   'Marilyn' => { 'age' => 23, 'gender' => 'female', 'age_group' => 'adult' } }
