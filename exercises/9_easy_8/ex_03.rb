fa548f29ab7178a1cf420a94a52a12281f93c627
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-12 13:32:51 +0000

    :ok_hand: Add shorter method for ex 9.3

--- exercises/9_easy_8/ex_03.rb
+++ exercises/9_easy_8/ex_03.rb
@@ -11,3 +11,13 @@ end
 p substrings_at_start('abc') == %w(a ab abc)
 p substrings_at_start('a') == ['a']
 p substrings_at_start('xyzzy') == %w(x xy xyz xyzz xyzzy)
+
+# Shorter method
+
+def substrings_at_start_2(string)
+  substrings = []
+  0.upto(string.size - 1) { |index| substrings << string[0..index] }
+  substrings
+end
+
+p substrings_at_start_2('abc')
