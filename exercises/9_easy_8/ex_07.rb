7e045394c528644dc4601d2ea126f932e227a1ae
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-12 15:17:27 +0000

    :rotating_light: Refactor according to Rubocop

--- exercises/9_easy_8/ex_07.rb
+++ exercises/9_easy_8/ex_07.rb
@@ -1,21 +1,25 @@
 def fizzbuzz(start_num, end_num)
   result = []
   start_num.upto(end_num) do |num|
-    result << if divisible_by(3, num) && divisible_by(5, num)
-                'FizzBuzz'
-              elsif divisible_by(3, num)
-                'Fizz'
-              elsif divisible_by(5, num)
-                'Buzz'
-              else
-                num
-              end
+    result << add_value_depending_on_divisibility(num)
   end
   p result.join(', ')
 end
 
 def divisible_by(divisor, num)
-  num % divisor == 0
+  (num % divisor).zero?
+end
+
+def add_value_depending_on_divisibility(num)
+  if divisible_by(3, num) && divisible_by(5, num)
+    'FizzBuzz'
+  elsif divisible_by(3, num)
+    'Fizz'
+  elsif divisible_by(5, num)
+    'Buzz'
+  else
+    num
+  end
 end
 
 fizzbuzz(1, 15)
