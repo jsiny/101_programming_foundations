afbc68b763d5366d4452c76e31d0b360077451e3
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-12 11:57:17 +0000

    :ok_hand: Add longer method for ex 9.1

--- exercises/9_easy_8/ex_01.rb
+++ exercises/9_easy_8/ex_01.rb
@@ -6,7 +6,23 @@ def sum_of_sums(numbers)
   sum
 end
 
-p sum_of_sums([3, 5, 2]) == 3 + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([3, 5, 2]) == 3 + (3 + 5) + (3 + 5 + 2) # 21
 p sum_of_sums([1, 5, 7, 3]) == 1 + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # 36
 p sum_of_sums([4]) == 4
 p sum_of_sums([1, 2, 3, 4, 5]) == 35
+
+# Other (longer) method
+
+def sum_of_sums_2(numbers)
+  sum_total = 0
+  accumulator = 0
+
+  numbers.each do |num|
+    accumulator += num
+    sum_total += accumulator
+  end
+
+  sum_total
+end
+
+p sum_of_sums_2([3, 5, 2])
