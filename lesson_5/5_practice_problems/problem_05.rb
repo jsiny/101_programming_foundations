27370d37a2a20ebde9032204a82d5abc0f5e415d
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-15 09:59:39 +0000

    :sparkles: Add practice problem 5

---
+++ lesson_5/5_practice_problems/problem_05.rb
@@ -0,0 +1,15 @@
+munsters = {
+  'Herman' => { 'age' => 32, 'gender' => 'male' },
+  'Lily' => { 'age' => 30, 'gender' => 'female' },
+  'Grandpa' => { 'age' => 402, 'gender' => 'male' },
+  'Eddie' => { 'age' => 10, 'gender' => 'male' },
+  'Marilyn' => { 'age' => 23, 'gender' => 'female' }
+}
+
+sum = 0
+
+munsters.each do |_, info|
+  sum += info['age'] if info['gender'] == 'male'
+end
+
+puts "The total age of the male members of the family is #{sum}."
