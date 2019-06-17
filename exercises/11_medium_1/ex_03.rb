d59597aace6321bc9ccc5b723b40fe5bf04b827e
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-17 08:26:53 +0000

    :ok_hand: Add shorter method for ex 11.3

--- exercises/11_medium_1/ex_03.rb
+++ exercises/11_medium_1/ex_03.rb
@@ -21,8 +21,34 @@ def max_rotation(num)
   result.join.to_i
 end
 
-p max_rotation(735291) == 321579
+p max_rotation(735_291) == 321_579
 p max_rotation(3) == 3
 p max_rotation(35) == 53
 p max_rotation(105) == 15 # the leading zero gets dropped
 p max_rotation(8_703_529_146) == 7_321_609_845
+
+# Shorter method
+
+def max_rotation_2(num)
+  number_digits = num.to_s.size
+  number_digits.downto(2) do |n|
+    num = rotate_rightmost_digits_2(num, n)
+  end
+  num
+end
+
+def rotate_rightmost_digits_2(num, n)
+  all_digits = num.to_s.chars
+  all_digits[-n..-1] = rotate_array_2(all_digits[-n..-1])
+  all_digits.join.to_i
+end
+
+def rotate_array_2(array)
+  array[1..-1] + [array[0]]
+end
+
+p max_rotation_2(735_291) == 321_579
+p max_rotation_2(3) == 3
+p max_rotation_2(35) == 53
+p max_rotation_2(105) == 15 # the leading zero gets dropped
+p max_rotation_2(8_703_529_146) == 7_321_609_845
