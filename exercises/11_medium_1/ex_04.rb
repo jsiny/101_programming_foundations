f5e762ea30b0dd3be8b24beb2e5a818ca686f709
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-18 08:19:03 +0000

    :recycle: Refactor ex 11.4

--- exercises/11_medium_1/ex_04.rb
+++ exercises/11_medium_1/ex_04.rb
@@ -1,25 +1,19 @@
-def generate_initial_hash(num)
+def generate_initial_hash(number_of_lights)
   lights = {}
-
-  i = 1
-  num.times do
-    lights[i] = true
-    i += 1
-  end
-
+  1.upto(number_of_lights) { |number| lights[number] = false }
   lights
 end
 
-def switch(num)
-  lights = generate_initial_hash(num)
+def switch(number_of_lights)
+  lights = generate_initial_hash(number_of_lights)
 
   counter = 1
-  (num - counter).times do
-    counter += 1
-
+  number_of_lights.times do
     lights.each do |number, boolean|
       lights[number] = !boolean if (number % counter).zero?
     end
+
+    counter += 1
   end
 
   lights.select { |_, boolean| boolean == true }.keys
@@ -31,3 +25,4 @@ p switch(2)   == [1]
 p switch(3)   == [1]
 p switch(5)   == [1, 4]
 p switch(10)  == [1, 4, 9]
+p switch(25)  == [1, 4, 9, 16, 25]
