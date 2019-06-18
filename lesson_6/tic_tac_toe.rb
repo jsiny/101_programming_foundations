4da7e4361429e371af182891f50b1294cf94ffee
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-18 14:50:53 +0000

    :sparkles: Add feature Display Board

---
+++ lesson_6/tic_tac_toe.rb
@@ -0,0 +1,24 @@
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = ' ' }
+  new_board
+end
+
+board = initialize_board
+display_board(board)
