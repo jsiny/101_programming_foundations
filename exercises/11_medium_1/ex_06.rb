45945ecae9f98afdf15126f2ac52e2676c4f3307
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-20 12:52:38 +0000

    :sparkles: Add Further Exploration ex 11.6

--- exercises/11_medium_1/ex_06.rb
+++ exercises/11_medium_1/ex_06.rb
@@ -21,34 +21,18 @@ end
 
 # Test cases
 
-minilang('PRINT')
-# 0
-
-minilang('5 PUSH 3 MULT PRINT')
-# 15
-
-minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
-# 5
-# 3
-# 8
-
-minilang('5 PUSH POP PRINT')
-# 5
-
+minilang('PRINT') # 0
+minilang('5 PUSH 3 MULT PRINT') # 15
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT') # 5, 3, 8
+minilang('5 PUSH POP PRINT') # 5
 minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
-# 5
-# 10
-# 4
-# 7
+# 5, 10, 4, 7
+minilang('3 PUSH PUSH 7 DIV MULT PRINT ') # 6
+minilang('4 PUSH PUSH 7 MOD MULT PRINT ') # 12
+minilang('-3 PUSH 5 SUB PRINT') # 8
+minilang('6 PUSH') # (nothing printed; no PRINT commands)
 
-minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
-# 6
+# Further Exploration
 
-minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
-# 12
-
-minilang('-3 PUSH 5 SUB PRINT')
+minilang('3 PUSH 5 MOD PUSH 7 PUSH 3 PUSH 4 PUSH 5 MULT ADD SUB DIV PRINT')
 # 8
-
-minilang('6 PUSH')
-# (nothing printed; no PRINT commands)
