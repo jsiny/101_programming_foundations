517bb800e2107fc4a53a9dcf072ed863cbe34cd9
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-21 14:39:34 +0000

    :rotating_light: Remove Rubocop warnings for ex 11.10

--- exercises/11_medium_1/ex_10.rb
+++ exercises/11_medium_1/ex_10.rb
@@ -1,18 +1,14 @@
 def fibonacci_last(n)
-  last_2 = [1, 1]
+  last2 = [1, 1]
   3.upto(n) do
-    last_2 = [last_2.last, (last_2.first + last_2.last) % 10]
+    last2 = [last2.last, (last2.first + last2.last) % 10]
   end
-  last_2.last
+  last2.last
 end
 
-# def fibonacci_last(n)
-#   fibonacci(n).digits.first
-# end
-
-p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
-p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
-p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
-p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
-p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
-p fibonacci_last(123456789) # -> 4
+p fibonacci_last(15)          # -> 0  (the 15th Fibonacci number is 610)
+p fibonacci_last(20)          # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(100)         # -> 5 (the 100th Fibonacci number is ...61915075)
+p fibonacci_last(100_001)     # -> 1 (this is a 20899 digit number)
+p fibonacci_last(1_000_007)   # -> 3 (this is a 208989 digit number)
+p fibonacci_last(123_456_789) # -> 4
