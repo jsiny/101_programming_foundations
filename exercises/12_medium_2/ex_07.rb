b5327bbaf077af09489523bffece2f0f38ece341
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-26 10:21:22 +0000

    :sparkles: Add ex 12.7

---
+++ exercises/12_medium_2/ex_07.rb
@@ -0,0 +1,16 @@
+require 'date'
+
+def friday_13th(year)
+  unlucky_days = 0
+  thirteenth = Date.new(year, 1, 13)
+  12.times do
+    unlucky_days += 1 if thirteen.friday?
+    thirteenth = thirteenth.next_month
+  end
+
+  unlucky_days
+end
+
+p friday_13th(2015) == 3
+p friday_13th(1986) == 1
+p friday_13th(2019) == 2
