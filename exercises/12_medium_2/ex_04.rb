10afe4996d910841bc40f1c47adc7a448c253851
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-26 07:25:05 +0000

    :recycle: Refactor original method ex 12.4

--- exercises/12_medium_2/ex_04.rb
+++ exercises/12_medium_2/ex_04.rb
@@ -1,22 +1,15 @@
-# rubocop:disable Metrics/MethodLength
 def balanced?(string)
   has_opening = 0
   has_closing = 0
 
   string.chars.each do |char|
-    if char == '('
-      has_opening += 1
-    elsif char == ')'
-      has_closing += 1
-    else
-      next
-    end
-    break false if has_closing > has_opening
+    has_opening += 1 if char == '('
+    has_closing += 1 if char == ')'
+    break if has_closing > has_opening
   end
 
   has_opening == has_closing
 end
-# rubocop: enable Metrics/MethodLength
 
 p balanced?('What (is) this?') == true
 p balanced?('What is) this?') == false
