0d81000d22d5b5c367e19b78431a7a228f2d9668
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-26 09:02:42 +0000

    :sparkles: Add Further Exploration ex 12.4

--- exercises/12_medium_2/ex_04.rb
+++ exercises/12_medium_2/ex_04.rb
@@ -1,42 +1,83 @@
-def balanced?(string)
-  has_opening = 0
-  has_closing = 0
+# def balanced_1?(string)
+#   has_opening = 0
+#   has_closing = 0
 
-  string.chars.each do |char|
-    has_opening += 1 if char == '('
-    has_closing += 1 if char == ')'
-    break if has_closing > has_opening
+#   string.chars.each do |char|
+#     has_opening += 1 if char == '('
+#     has_closing += 1 if char == ')'
+#     break if has_closing > has_opening
+#   end
+
+#   has_opening == has_closing
+# end
+
+# p balanced_1?('What (is) this?') == true
+# p balanced_1?('What is) this?') == false
+# p balanced_1?('What (is this?') == false
+# p balanced_1?('((What) (is this))?') == true
+# p balanced_1?('((What)) (is this))?') == false
+# p balanced_1?('Hey!') == true
+# p balanced_1?(')Hey!(') == false
+# p balanced_1?('What ((is))) up(') == false
+
+# # Shorter method
+
+# def balanced_2?(string)
+#   parentheses = 0
+#   string.each_char do |char|
+#     parentheses += 1 if char == '('
+#     parentheses -= 1 if char == ')'
+#     break if parentheses < 0
+#   end
+#   parentheses.zero?
+# end
+
+# p balanced_2?('What (is) this?') == true
+# p balanced_2?('What is) this?') == false
+# p balanced_2?('What (is this?') == false
+# p balanced_2?('((What) (is this))?') == true
+# p balanced_2?('((What)) (is this))?') == false
+# p balanced_2?('Hey!') == true
+# p balanced_2?(')Hey!(') == false
+# p balanced_2?('What ((is))) up(') == false
+
+# Further Exploration
+
+DIFFERENT_PAIRS = [%w(( )), %w([ ]), %w({ })].freeze
+QUOTES = ['\'', '\"'].freeze
+
+def extended_balance?(string)
+  result = []
+
+  DIFFERENT_PAIRS.each do |pair|
+    result << balanced?(string, pair)
   end
 
-  has_opening == has_closing
+  QUOTES.each do |quote|
+    result << balanced_with_quote?(string, quote)
+  end
+
+  result.all? { |element| element == true }
 end
 
-p balanced?('What (is) this?') == true
-p balanced?('What is) this?') == false
-p balanced?('What (is this?') == false
-p balanced?('((What) (is this))?') == true
-p balanced?('((What)) (is this))?') == false
-p balanced?('Hey!') == true
-p balanced?(')Hey!(') == false
-p balanced?('What ((is))) up(') == false
-
-# Shorter method
-
-def balanced_2?(string)
-  parentheses = 0
+def balanced?(string, pair)
+  duo = 0
   string.each_char do |char|
-    parentheses += 1 if char == '('
-    parentheses -= 1 if char == ')'
-    break if parentheses < 0
+    duo += 1 if char == pair[0]
+    duo -= 1 if char == pair[1]
+    break if duo < 0
   end
-  parentheses.zero?
+  duo.zero?
 end
 
-p balanced_2?('What (is) this?') == true
-p balanced_2?('What is) this?') == false
-p balanced_2?('What (is this?') == false
-p balanced_2?('((What) (is this))?') == true
-p balanced_2?('((What)) (is this))?') == false
-p balanced_2?('Hey!') == true
-p balanced_2?(')Hey!(') == false
-p balanced_2?('What ((is))) up(') == false
+def balanced_with_quote?(string, quote)
+  string.chars.count { |char| char == quote }.even?
+end
+
+p extended_balance?('hey {') == false
+p extended_balance?('hello (madam) I will be {your} guide') == true
+p extended_balance?("hello (madam) I'll be {your} guide") == false
+p extended_balance?('hello (madam') == false
+p extended_balance?('} next {') == false
+p extended_balance?("I 'love' you") == true
+p extended_balance?('I "would" love to meet ya') == true
