2b0f71a8ec88d4466a257901a30d8c830bc21a61
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-26 10:00:25 +0000

    :recycle: Refactor ex 12.6

--- exercises/12_medium_2/ex_06.rb
+++ exercises/12_medium_2/ex_06.rb
@@ -1,24 +1,21 @@
+# rubocop:disable Style/EmptyCaseCondition
 def triangle(a, b, c)
   angles = [a, b, c]
 
-  if valid_triangle?(angles)
+  if angles.sum != 180 || angles.include?(0)
+    :invalid
+  else
     case
     when angles.include?(90)                then :right
     when angles.all? { |angle| angle < 90 } then :acute
     when angles.any? { |angle| angle > 90 } then :obtuse
     end
-  else
-    :invalid
   end
 end
+# rubocop:enable Style/EmptyCaseCondition
 
-def valid_triangle?(angles)
-  return false if angles.include?(0)
-  angles.sum == 180
-end
-
-p triangle(60, 70, 50) == :acute
-p triangle(30, 90, 60) == :right
+p triangle(60, 70, 50)  == :acute
+p triangle(30, 90, 60)  == :right
 p triangle(120, 50, 10) == :obtuse
-p triangle(0, 90, 90) == :invalid
-p triangle(50, 50, 50) == :invalid
+p triangle(0, 90, 90)   == :invalid
+p triangle(50, 50, 50)  == :invalid
