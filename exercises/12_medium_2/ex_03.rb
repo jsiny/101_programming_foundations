1edc6dd46894837eba86f9fa8651596c2196b2ef
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-26 06:58:30 +0000

    :sparkles: Add Further Exploration ex 12.3

--- exercises/12_medium_2/ex_03.rb
+++ exercises/12_medium_2/ex_03.rb
@@ -11,7 +11,7 @@ def letter_percentages(string)
 end
 
 def convert_to_percentage(hash, key, length)
-  hash[key] = (hash[key] / length) * 100
+  hash[key] = ((hash[key] / length) * 100).round(2)
 end
 
 p letter_percentages('h7ag ') == { lowercase: 60, uppercase: 0, neither: 40 }
@@ -20,3 +20,5 @@ p letter_percentages('abCdef 123') == { lowercase: 50, uppercase: 10,
 p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5,
                                       neither: 25 }
 p letter_percentages('123') == { lowercase: 0, uppercase: 0, neither: 100 }
+p letter_percentages('abcdefGHI') == {  lowercase: 66.67, uppercase: 33.33,
+                                        neither: 0 }
