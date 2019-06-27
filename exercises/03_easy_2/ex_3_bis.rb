4aee668700ef5a1c7061fb2540c87d161792e91e
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-27 07:59:21 +0000

    :sparkles: Add Further Exploration ex 3.3

--- exercises/03_easy_2/ex_3_bis.rb
+++ exercises/03_easy_2/ex_3_bis.rb
@@ -1,11 +1,13 @@
+TWO_DECIMALS = '%.2f'.freeze
+
 print 'What is the bill? '
 before_tip = gets.to_f
 
 print 'What is the tip percentage? '
 tip_percentage = gets.to_f
 
-tip = (before_tip * tip_percentage / 100).round(2)
-total_bill = (before_tip + tip).round(2)
+tip = (before_tip * tip_percentage / 100)
+total_bill = (before_tip + tip)
 
-puts "The tip is $#{tip}"
-puts "The total is $#{total_bill}"
+puts "The tip is $#{TWO_DECIMALS % tip}"
+puts "The total is $#{TWO_DECIMALS % total_bill}"
