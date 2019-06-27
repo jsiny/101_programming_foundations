b5b44ed848e2782c467d68b9848eef95bdb1a2d0
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-27 07:45:11 +0000

    :sparkles: Add Further Exploration for ex 2.7

--- exercises/02_easy_1/ex_7_bis.rb
+++ exercises/02_easy_1/ex_7_bis.rb
@@ -1,10 +1,24 @@
-def stringy(n)
+def stringy_1(n)
   string = ''
-  (1..n).each { |i| string += i.odd? ? '1' : '0' }
+  n.times { |i| string += i.even? ? '1' : '0' }
   string
 end
 
-puts stringy(6) == '101010'
-puts stringy(9) == '101010101'
-puts stringy(4) == '1010'
-puts stringy(7) == '1010101'
+puts stringy_1(6) == '101010'
+puts stringy_1(9) == '101010101'
+puts stringy_1(4) == '1010'
+puts stringy_1(7) == '1010101'
+
+# Further Exploration
+
+def stringy(n, even = 1)
+  string = ''
+  odd = even.zero? ? 1 : 0
+  n.times { |i| string += i.even? ? even.to_s : odd.to_s }
+  string
+end
+
+puts stringy(6, 0)  == '010101'
+puts stringy(9)     == '101010101'
+puts stringy(4, 0)  == '0101'
+puts stringy(7)     == '1010101'
