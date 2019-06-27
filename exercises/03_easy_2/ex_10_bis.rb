a4feff4575f1ad9c0b5750a97314b1b7486e6e3c
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-27 10:00:55 +0000

    :sparkles: Add new version to ex 3.10

---
+++ exercises/03_easy_2/ex_10_bis.rb
@@ -0,0 +1,6 @@
+array1 = %w(Moe Larry Curly Shemp Harpo Chico Groucho Zeppo)
+array2 = []
+array1.each { |value| array2 << value }
+array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
+
+p array2 == %w(Moe Larry CURLY SHEMP Harpo CHICO Groucho Zeppo)
