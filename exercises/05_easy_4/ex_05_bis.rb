912c2cce5f6626c19ebc61a22d04ddcaf05dd8ee
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-28 07:33:51 +0000

    :sparkles: Add Further Exploration ex 5.5

--- exercises/05_easy_4/ex_05_bis.rb
+++ exercises/05_easy_4/ex_05_bis.rb
@@ -1,11 +1,22 @@
 require 'pry'
 
-def multisum(num)
+def multisum_1(num)
   sum = 0
   1.upto(num) do |i|
     sum += i if (i % 3).zero? || (i % 5).zero?
   end
   sum
+end
+
+p multisum_1(3) == 3
+p multisum_1(5) == 8
+p multisum_1(10) == 33
+p multisum_1(1000) == 234_168
+
+# Further Exploration
+
+def multisum(num)
+  (1..num).select { |i| (i % 3).zero? || (i % 5).zero? }.reduce(:+)
 end
 
 p multisum(3) == 3
