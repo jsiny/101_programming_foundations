71ad7825d4fe0dbe50ea9a282150fdae9ee41c0c
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-28 07:14:55 +0000

    :sparkles: Add new version for ex 4.9

---
+++ exercises/04_easy_3/ex_09_bis.rb
@@ -0,0 +1,20 @@
+def palindrome?(string)
+  string == string.reverse
+end
+
+p palindrome?('madam') == true
+p palindrome?('Madam') == false          # (case matters)
+p palindrome?("madam i'm adam") == false # (all characters matter)
+p palindrome?('356653') == true
+
+def real_palindrome?(string)
+  simplified_string = string.downcase.delete('^a-z0-9')
+  palindrome?(simplified_string)
+end
+
+p real_palindrome?('madam') == true
+p real_palindrome?('Madam') == true           # (case does not matter)
+p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+p real_palindrome?('356653') == true
+p real_palindrome?('356a653') == true
+p real_palindrome?('123ab321') == false
