27fd843b7b58fd57e0e4bfc28cd3844d338e9da1
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-28 09:08:26 +0000

    :sparkles: Add Further Exploration in ex 5.7

--- exercises/05_easy_4/ex_07_bis.rb
+++ exercises/05_easy_4/ex_07_bis.rb
@@ -19,3 +19,23 @@ end
 
 p string_to_integer('4321') == 4321
 p string_to_integer('570') == 570
+
+# Further Exploration
+
+HEX_TO_DECIMAL = {  '0' => 0,   '1' => 1,   '2' => 2,   '3' => 3,   '4' => 4,
+                    '5' => 5,   '6' => 6,   '7' => 7,   '8' => 8,   '9' => 9,
+                    'A' => 10,  'B' => 11,  'C' => 12,  'D' => 13,  'E' => 14,
+                    'F' => 15 }.freeze
+
+def hexadecimal_to_integer(hex_string)
+  decimal_int = 0
+  hex_string.chars.reverse.each_with_index do |hex_str, index|
+    decimal_int += HEX_TO_DECIMAL[hex_str.upcase] * (16**index)
+  end
+  decimal_int
+end
+
+p hexadecimal_to_integer('10') == 16
+p hexadecimal_to_integer('1a') == 26
+p hexadecimal_to_integer('4D9f') == 19_871
+# 4 * 16^3 + 13 * 16^2 + 9 * 16 + 15
