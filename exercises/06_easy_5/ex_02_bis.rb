132b198e1c5b0e05cf5b065e97f8c773506f0914
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-29 14:53:13 +0000

    :ok_hand: Refactor program with divmod method and shorter Kernel#format invokation

--- exercises/06_easy_5/ex_02_bis.rb
+++ exercises/06_easy_5/ex_02_bis.rb
@@ -3,15 +3,14 @@ HOURS_IN_A_DAY = 24
 
 def time_of_day(integer)
   total_minutes = integer % (MINUTES_IN_AN_HOUR * HOURS_IN_A_DAY)
-  hours = total_minutes / MINUTES_IN_AN_HOUR
-  minutes = total_minutes % MINUTES_IN_AN_HOUR
-  format("%02d", hours) + ':' + format("%02d", minutes)
+  hours, minutes = total_minutes.divmod(MINUTES_IN_AN_HOUR)
+  format('%02d:%02d', hours, minutes)
 end
 
-p time_of_day(0) == "00:00"
-p time_of_day(-3) == "23:57"
-p time_of_day(35) == "00:35"
-p time_of_day(-1437) == "00:03"
-p time_of_day(3000) == "02:00"
-p time_of_day(800) == "13:20"
-p time_of_day(-4231) == "01:29"
+p time_of_day(0)      == '00:00'
+p time_of_day(-3)     == '23:57'
+p time_of_day(35)     == '00:35'
+p time_of_day(-1437)  == '00:03'
+p time_of_day(3000)   == '02:00'
+p time_of_day(800)    == '13:20'
+p time_of_day(-4231)  == '01:29'
