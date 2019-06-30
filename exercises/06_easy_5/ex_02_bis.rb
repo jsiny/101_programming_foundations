fe24710dacebefbb55d9b455cc43f4fce033e875
Juliette Sinibardy <juju.siny@gmail.com>
2019-06-30 17:02:50 +0000

    :sparkles: Add Further Exploration ex 6.2

--- exercises/06_easy_5/ex_02_bis.rb
+++ exercises/06_easy_5/ex_02_bis.rb
@@ -14,3 +14,33 @@ p time_of_day(-1437)  == '00:03'
 p time_of_day(3000)   == '02:00'
 p time_of_day(800)    == '13:20'
 p time_of_day(-4231)  == '01:29'
+
+# Further Exploration: with Date and Time modules
+
+SECONDS_PER_MINUTES = 60
+
+def time_of_day_2(integer)
+  (Time.new(2019) + integer * SECONDS_PER_MINUTES).strftime('%H:%M')
+end
+
+p time_of_day_2(0)      == '00:00'
+p time_of_day_2(-3)     == '23:57'
+p time_of_day_2(35)     == '00:35'
+p time_of_day_2(-1437)  == '00:03'
+p time_of_day_2(3000)   == '02:00'
+p time_of_day_2(800)    == '13:20'
+p time_of_day_2(-4231)  == '01:29'
+
+# Further Exploration: with days of the week
+
+def time_and_date(n)
+  (Time.new(2019, 6, 30) + n * SECONDS_PER_MINUTES).strftime('%A %R')
+end
+
+p time_and_date(0)      == 'Sunday 00:00'
+p time_and_date(-3)     == 'Saturday 23:57'
+p time_and_date(35)     == 'Sunday 00:35'
+p time_and_date(-1437)  == 'Saturday 00:03'
+p time_and_date(3000)   == 'Tuesday 02:00'
+p time_and_date(800)    == 'Sunday 13:20'
+p time_and_date(-4231)  == 'Thursday 01:29'
