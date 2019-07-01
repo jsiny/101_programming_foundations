12d5254e6e34f3230d30865a330d3a1365a11537
Juliette Sinibardy <juju.siny@gmail.com>
2019-07-01 08:51:15 +0000

    :sparkles: Add new version for ex 11.3

--- exercises/11_medium_1/ex_03_bis.rb
+++ exercises/11_medium_1/ex_03_bis.rb
@@ -1,5 +1,23 @@
+def rotate_array(arr)
+  arr[1..-1] + [arr[0]]
+end
 
+def rotate_rightmost_digits(number, n)
+  digits = number.digits.reverse
+  digits[-n..-1] = rotate_array(digits[-n..-1])
+  digits.join.to_i
+end
 
+def max_rotation(number)
+  size = number.to_s.size
+
+  while size > 0
+    number = rotate_rightmost_digits(number, size)
+    size -= 1
+  end
+
+  number
+end
 
 p max_rotation(735_291) == 321_579
 p max_rotation(3) == 3
