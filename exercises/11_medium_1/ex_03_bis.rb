49d762f95a35bd2f692d715ae224c8f451060e36
Juliette Sinibardy <juju.siny@gmail.com>
2019-07-01 08:11:46 +0000

    :sparkles: Add new version for ex 11.1

---
+++ exercises/11_medium_1/ex_03_bis.rb
@@ -0,0 +1,8 @@
+
+
+
+p max_rotation(735_291) == 321_579
+p max_rotation(3) == 3
+p max_rotation(35) == 53
+p max_rotation(105) == 15 # the leading zero gets dropped
+p max_rotation(8_703_529_146) == 7_321_609_845
