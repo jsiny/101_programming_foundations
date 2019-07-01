a130706f4055a470bbacdb509fd423e5376828ff
Juliette Sinibardy <juju.siny@gmail.com>
2019-07-01 07:32:23 +0000

    :sparkles: Add new version for ex 7.8

---
+++ exercises/08_easy_7/ex_08_bis.rb
@@ -0,0 +1,11 @@
+def staggered_case(string)
+  sentence = ''
+  string.chars.each_with_index do |char, i|
+    sentence += i.even? ? char.upcase : char.downcase
+  end
+  sentence
+end
+
+p staggered_case('I Love Launch School!')     == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS')                  == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
