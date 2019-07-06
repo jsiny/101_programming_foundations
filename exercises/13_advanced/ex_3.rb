a79de8a701b0a73b3fe87bad6b36d109827fe262
Juliette Sinibardy <juju.siny@gmail.com>
2019-07-06 15:07:26 +0000

    :ok_hand: Add more elegant method for ex 13.3

--- exercises/13_advanced/ex_3.rb
+++ exercises/13_advanced/ex_3.rb
@@ -22,3 +22,21 @@ new_matrix = transpose(matrix)
 
 p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
 p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+
+# More elegant method
+
+def transpose_1(matrix)
+  result = []
+
+  (0..2).each do |column_index|
+    new_row = (0..2).map { |row_index| matrix[row_index][column_index] }
+    result << new_row
+  end
+
+  result
+end
+
+new_matrix = transpose_1(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
