b77f6e7e27c46ced19d19ee36bfbfc08ced96d7a
Juliette Sinibardy <juju.siny@gmail.com>
2019-07-16 08:40:12 +0000

    :sparkles: Add ex 4.3 to 4.4

---
+++ exercises/04_easy_3/ex_03_ter.rb
@@ -0,0 +1,4 @@
+print 'Please write word or multiple words: '
+word = gets.chomp
+
+puts "There are #{word.count('^ ')} characters in #{word}."
---
+++ exercises/04_easy_3/ex_04_ter.rb
@@ -0,0 +1,6 @@
+def multiply(x, y)
+  x * y
+end
+
+p multiply(3, 5)
+p multiply([3, 2], 5)
