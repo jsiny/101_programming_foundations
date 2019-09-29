1c6c9d6b9514ccf4da13f186cbf3a24565bd0155
Juliette Sinibardy <juju.siny@gmail.com>
2019-09-29 19:42:01 +0200

    :sparkles: Add ex 14.5

---
+++ exercises/14_debugging/ex_05.rb
@@ -0,0 +1,35 @@
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
+
+deck = { :hearts   => cards.clone,
+         :diamonds => cards.clone,
+         :clubs    => cards.clone,
+         :spades   => cards.clone }
+
+def score(card)
+  case card
+  when :ace   then 11
+  when :king  then 10
+  when :queen then 10
+  when :jack  then 10
+  else card
+  end
+end
+
+# Pick one random card per suit
+
+player_cards = []
+deck.keys.each do |suit|
+  cards = deck[suit]
+  cards.shuffle!
+  player_cards << cards.pop
+end
+
+# Determine the score of the remaining cards in the deck
+
+sum = deck.reduce(0) do |_, (_, remaining_cards)|
+  remaining_cards.map do |card|
+    score(card)
+  end.sum
+end
+
+puts sum
